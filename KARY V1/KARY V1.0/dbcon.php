f4057744a2bbdcef4fe7731624e0708227577df0
Pd011161 <78379979+Pd011161@users.noreply.github.com>
2022-10-26 16:28:34 +0700

    Delete KARY V1/KARY V1.0 directory

--- KARY V1/KARY V1.0/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","","Kary_DB");
-
-if(!$con){
-    die('Connection Failed'. mysqli_connect_error());
-}
-
-?>
--- KARY V1/KARY V1.0/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start(); //ประกาศใช้ session
-session_destroy(); //เคลียร์ค่า session
- header('Location: login.php'); //Logout เรียบร้อยและกระโดดไปหน้าตามที่ต้องการ
-//devbanban.com
-?>
